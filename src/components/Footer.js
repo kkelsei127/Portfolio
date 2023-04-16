b01111ede1e1a03eeecaa0c7828d31eda997dd7a
Kelsei Kyser <kkelsei127@yahoo.com>
2023-04-16 00:03:07 -0400

    fixed footer, made it one row and fit on page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,19 @@ function Footer () {
     return (
         <div>
             <footer className="footer">
-                    <div className="row">
-                        <div className="col-md-6 ftr">
-                            <h6>Computer crafted with lot's of time and love!</h6>
-                        </div>
-                        <div className="col-md-6 icons">
-                            <div className="icons">
-                            <a href="https://github.com/kkelsei127" style={{color: "#ce8cb1c1"}}><AiFillGithub/></a>
-                            <a href="https://www.linkedin.com/in/kelsei-kyser/" style={{color: "#ce8cb1c1"}}><AiFillLinkedin/></a>
-                            </div>
+                <div className="row">
+                    <div className="col-md-6 ftr">
+                        <p>Crafted with lots of time and love!</p>
+                    </div>
+                    <div className="col-md-6 icons">
+                        <div className="icons">
+                        <a href="https://github.com/kkelsei127" style={{color: "#ce8cb1c1"}}><AiFillGithub/></a>
+                        <a href="https://www.linkedin.com/in/kelsei-kyser/" style={{color: "#ce8cb1c1"}}><AiFillLinkedin/></a>
                         </div>
                     </div>
-                </footer>
+                    
+                </div>
+            </footer>
         </div>
     );
 }

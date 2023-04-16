8eeceb36e8d53e9b4a4c4507371ead433ef9fc2a
Kelsei Kyser <kkelsei127@yahoo.com>
2023-04-15 22:52:29 -0400

    corrected responsiveness in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ function Footer () {
         <div>
             <footer className="footer">
                     <div className="row">
-                        <div className="col-md ftr">
+                        <div className="col-md-6 ftr">
                             <h6>Computer crafted with lot's of time and love!</h6>
                         </div>
-                        <div className="col-sm icons">
+                        <div className="col-md-6 icons">
                             <div className="icons">
                             <a href="https://github.com/kkelsei127" style={{color: "#ce8cb1c1"}}><AiFillGithub/></a>
                             <a href="https://www.linkedin.com/in/kelsei-kyser/" style={{color: "#ce8cb1c1"}}><AiFillLinkedin/></a>
